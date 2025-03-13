895b958a1b44bfa479cbf9ab3535f765b0a9c421
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2025-03-13 16:35:16 -0300

    Possible moves of a piece

--- src/chesslayer/ChessMatch.java
+++ src/chesslayer/ChessMatch.java
@@ -49,7 +49,10 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
-			throw new ChessException("There's no piece on the source position");
+			throw new ChessException("There's no piece on the source position.");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There's no possible move for the chosen piece.");
 		}
 	}
 	
